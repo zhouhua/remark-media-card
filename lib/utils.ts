e3486cdeb45eaa0115ec0a884b02894c5fee41ad
Zhou Hua <zhou--hua@163.com>
2024-02-22 11:17:26 +0800

    修复gatsby-image的兼容问题

--- lib/utils.ts
+++ lib/utils.ts
@@ -65,74 +65,73 @@ export function template(cardMeta: ICardMeta): string {
 
   return html`
     <div class="media-card-wrap" style="margin: 0 auto;">
-      ${url ? `<a href="${url}" targer="_blank">` : ''}
+      ${url ? html`<a href="${url}" target="_blank"></a>` : ''}
       <div
         class="media-card"
         style="
-                max-width: 100%;
-                ${width ? `width: ${width}px;` : ''}
-                display:flex;
-                flex-direction: column;
-                margin:30px 20px;
-                padding: 15px;
-                border-radius: 15px;
-                position: relative;
-                overflow: hidden;
-                text-decoration: none;
-              "
+          max-width: 100%;
+          ${width ? `width: ${width}px;` : ''}
+          display:flex;
+          flex-direction: column;
+          margin:30px 20px;
+          padding: 15px;
+          border-radius: 15px;
+          position: relative;
+          overflow: hidden;
+          text-decoration: none;
+        "
       >
         <div
           class="media-card-bg"
           style="
-                width: 110%;
-                height: 110%;
-                position: absolute;
-                top: 0;
-                left: 0;
-                z-index: -1;
-                border-radius: 15px;
-                background-color: antiquewhite;
-                background-image: url(${cover});
-                background-size: cover;
-                background-position: center;
-                background-repeat: no-repeat;
-                filter: blur(15px) brightness(0.6);
-              "
+            width: 110%;
+            height: 110%;
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: -1;
+            border-radius: 15px;
+            background-color: antiquewhite;
+            background-image: url(${cover});
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            filter: blur(15px) brightness(0.6);
+          "
         ></div>
         <div
           class="media-card-main"
           style="
-                display: flex;
-                flex-direction: column;
-                font-family: 'Courier New', Courier, monospace;
-                color: antiquewhite;
-              "
+            display: flex;
+            flex-direction: column;
+            font-family: 'Courier New', Courier, monospace;
+            color: antiquewhite;
+          "
         >
           <div
             class="media-card-meta"
             style="
-                  display: flex;
-                  align-items: center;
-                  gap: 12px;
-                "
+              display: flex;
+              align-items: center;
+              gap: 12px;
+            "
           >
-            <img
+            <div
               class="media-card-cover"
-              src="${cover}"
               style="
-                      height: 130px;
-                      width: 80px;
-                      object-fit: cover;
-                      object-position: center;
-                  "
-            />
+                height: 130px;
+                width: 80px;
+              "
+            >
+              <img src="${cover}" alt="cover" />
+            </div>
             <div
               style="
-                      display: flex;
-                      flex-direction: column;
-                      gap: 6px;
-                      line-height: 1.4;
-                  "
+                display: flex;
+                flex-direction: column;
+                gap: 6px;
+                line-height: 1.4;
+              "
             >
               ${cardItems.map((string_) => `<div>${string_}</div>`).join('')}
             </div>
@@ -141,20 +140,20 @@ export function template(cardMeta: ICardMeta): string {
             ? html`<div
                 class="media-card-introduction"
                 style="
-                        overflow: hidden;
-                        text-overflow: ellipsis;
-                        white-space: pre-line;
-                        -webkit-box-orient: vertical;
-                        -webkit-line-clamp: 3;
-                        display: -webkit-box;
-                  "
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: pre-line;
+                -webkit-box-orient: vertical;
+                -webkit-line-clamp: 3;
+                display: -webkit-box;
+              "
               >
                 ${introduction}
               </div>`
             : ''}
         </div>
       </div>
-      ${url ? '</a>' : ''}
+      ${url ? html`</a>` : ''}
     </div>
   `;
 }
